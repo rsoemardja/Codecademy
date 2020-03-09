60f9cb773f9e04d2683757f042bdeee17935255d
rsoemardja <rsoemardja@gmail.com>
2020-03-09 18:23:02 +1300

    CodeCademy Leanr Ruby
    Putting the From in Formatter tasks  with Ruby Scripting

---
+++ Ruby/Learn Ruby/Putting the Form in Formatter/Printing the Output.rb
@@ -0,0 +1,12 @@
+print "What's your first name? "
+first_name = "Rama"
+puts "Your name is #{first_name}!" #This will print the output from the first_name
+
+print "What's your last name? "
+last_name = gets.chomp
+
+print "What city are you from? "
+city = gets.chomp
+
+print "What state or province are you from? "
+state = gets.chomp
