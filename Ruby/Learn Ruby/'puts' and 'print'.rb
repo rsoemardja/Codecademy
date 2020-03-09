9b32babf5e1ff98e6d1e86702edb4fedd60a878f
rsoemardja <rsoemardja@gmail.com>
2020-03-09 17:22:05 +1300

    Learn Ruby
    Learning the Fundamentals of Ruby Scripting

---
+++ Ruby/Learn Ruby/'puts' and 'print'.rb
@@ -0,0 +1,7 @@
+puts "What's up?"
+print "Oxnard Montalvo"
+
+puts "What's your name?"
+print "Rama"
+
+print "Hello"
---
+++ Ruby/Learn Ruby/Math.rb
@@ -0,0 +1,11 @@
+2 + 3 #Addition
+
+2 - 3 #Subtraction
+
+2 * 3 #Multiplication
+
+6 / 3 #Division
+
+2**3 #Exponent
+
+25 % 7 #Modulo / Modulus
