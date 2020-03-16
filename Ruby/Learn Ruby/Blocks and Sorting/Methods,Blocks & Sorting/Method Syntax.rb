8f5b09979edf241c0a3cb17205b6aa5c382e2e2d
rsoemardja <rsoemardja@gmail.com>
2020-03-16 23:07:09 +1300

    Codecademy Learn Ruby
    30% of Learn Ruby

---
+++ Ruby/Learn Ruby/Blocks and Sorting/Methods,Blocks & Sorting/Method Syntax.rb
@@ -0,0 +1,18 @@
+#example
+def welcome
+  puts "Welcome to Ruby!"
+end
+
+# This is the problem fill the blanks for the method syntax
+___ puts_1_to_10
+  (1..10).each { |i| puts i }
+___
+
+puts_1_to_10 # Ignore this for now. We'll explain it soon!
+
+#Methods
+def puts_1_to_10
+  (1..10).each { |i| puts i }
+end
+
+puts_1_to_10 # Ignore this for now. We'll explain it soon!
