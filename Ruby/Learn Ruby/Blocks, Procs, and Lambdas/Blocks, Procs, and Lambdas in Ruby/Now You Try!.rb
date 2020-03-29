ca4c5e5068e54897120039aff8598e891dbd6be3
rsoemardja <rsoemardja@gmail.com>
2020-03-29 23:59:28 +1300

    CodeCademy Ruby
    I Did Now You try and updated it and now it is day 22 of this and hopefully will finish this tomorrow

---
+++ Ruby/Learn Ruby/Blocks, Procs, and Lambdas/Blocks, Procs, and Lambdas in Ruby/Now You Try!.rb
@@ -0,0 +1,10 @@
+# Now we will implement our own Lambda and pass it to a method.
+# IT will be exactly a lot like with what we had done with procs.
+# and we will need to put a & at the beginning of our Lambda name when we pass it to the method.
+
+
+# The word Symbol HAS to be capitalized when you're doing an .is_a check!
+:hello.is_a? Symbol
+# ==> true
+
+# in the solution we will create a Lambda Symbol filter that takes 
